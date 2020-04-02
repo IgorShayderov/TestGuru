891724664f0838c8c2003e23308a130516818536
IgorShayderov <flashmob123@gmail.com>
2020-04-02 14:52:09 +0200

    forgotten changes

--- db/migrate/20200401225409_create_answers.rb
+++ db/migrate/20200401225409_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.string :body
-      t.integer :test_id
+      t.integer(:test_id).references(:tests)
       t.correct :boolean
 
       t.timestamps
