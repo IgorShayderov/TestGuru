42fa59acd6db7337207d66875f984ce6f16dcf03
IgorShayderov <flashmob123@gmail.com>
2020-04-03 09:36:03 +0200

    3

--- db/migrate/20200401225409_create_answers.rb
+++ db/migrate/20200401225409_create_answers.rb
@@ -2,11 +2,10 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.string :body
-      t.integer :test_id
       t.boolean :correct
+      t.references :question, foreign_key: true
 
       t.timestamps
     end
-    add_reference :answers, :questions, foreign_key: true
   end
 end
--- db/migrate/20200403062722_add_tests_categories_reference.rb
+++ db/migrate/20200403062722_add_tests_categories_reference.rb
@@ -1,5 +1,5 @@
 class AddTestsCategoriesReference < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :tests, :categories, foreign_key: true
+  def up
+    add_reference :tests, :category, foreign_key: true
   end
 end
