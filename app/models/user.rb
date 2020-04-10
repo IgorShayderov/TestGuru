d793e8c92cd1ed09219b77771b622c832242dee2
IgorShayderov <flashmob123@gmail.com>
2020-04-10 19:39:37 +0200

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   def tests_by_level(level)
     Test
-      .joins("JOIN users_tests ON tests.id = users_tests.test_id")
+      .joins("JOIN tests_users ON tests.id = tests_users.test_id")
       .where("user_id = :id AND level = :level", id: id, level: level)
   end
 
