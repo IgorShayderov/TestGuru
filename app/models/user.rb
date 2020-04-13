861d11a7178dbbeca1dd18f99695c9a093eb6b51
IgorShayderov <flashmob123@gmail.com>
2020-04-13 15:33:08 +0200

    fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
 
-  has_many :tests, inverse_of: 'author'
+  has_many :created_tests, :class_name => "Test", inverse_of: 'author'
   has_many :tests_users
   has_many :tests, through: :tests_users, dependent: :destroy
 
   def tests_by_level(level)
-    Test
-      .joins(:tests_users)
-      .where(tests_users: {user_id: id}, level: level)
+      created_tests
+      .where(level: level)
   end
 
 end
