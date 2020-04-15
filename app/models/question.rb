b7adb54d5e0ddd4323b2368c0f125734efa6266e
IgorShayderov <flashmob123@gmail.com>
2020-04-15 19:40:12 +0200

    hz

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,11 +4,6 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :body, presence: true
-  validates :answers, length: { minimum: 1, maximum: 4 }
-  validate :validate_tags
-
-  def validate_tags
-    errors.add(:answers, "too much") if answers.size > 4
-  end
+  validates :answers, length: { minimum: 1, maximum: 4 }, on: :create
 
 end
