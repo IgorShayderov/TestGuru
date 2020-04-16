0abdcadae8f9c50d1479b951ab5a0f65c2ac938a
IgorShayderov <flashmob123@gmail.com>
2020-04-16 16:43:45 +0200

    fixed

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,6 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :body, presence: true
-  validates :answers, length: { minimum: 1, maximum: 4 }, on: :create
+  validates :answers, length: { minimum: 1, maximum: 4 }
 
 end
