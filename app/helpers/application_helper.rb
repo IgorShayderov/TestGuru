4d58bc9e748082ae60c3792a18fab870c3264f26
IgorShayderov <flashmob123@gmail.com>
2020-04-21 19:43:28 +0200

    Finished footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def current_year
+    Time.zone.now.year
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+  def question_header(question)
+
+    headers = {
+      "edit" => "Edit",
+      "new" => "Create new",
+    }
+    "#{headers[params[:action]]} #{question.test.title} Question"
+  end
 end
