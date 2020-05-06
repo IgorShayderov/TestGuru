332e21940e0ff1c56738b3d682e3979753224c4f
Igor <39465013+IgorShayderov@users.noreply.github.com>
2020-05-06 21:44:53 +0200

    Update app/controllers/application_controller.rb
    Co-authored-by: max-underthesun <max.underthesun@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(user)
-    current_user.admin? ? admin_tests_path : tests_path
+    user.admin? ? admin_tests_path : tests_path
   end
 
   def set_locale
