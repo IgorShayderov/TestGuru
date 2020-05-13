e4aa518a5c2fe1b358369d1a490720eb6cac8759
IgorShayderov <flashmob123@gmail.com>
2020-05-13 13:05:24 +0200

    added jquery

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :category
@@ -9,7 +11,7 @@ class Test < ApplicationRecord
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
 
-  scope :tests_by_category_title, -> (category_title) { joins(:category).where(categories: { title: category_title }).order(title: :desc) }
+  scope :tests_by_category_title, ->(category_title) { joins(:category).where(categories: { title: category_title }).order(title: :desc) }
 
   validates :title, presence: true
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
